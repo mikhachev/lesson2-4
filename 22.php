9b9fb73b6feabfb2c79e45939b417eb093bf66b7
mikhachev <36811961+mikhachev@users.noreply.github.com>
2018-04-13 21:31:47 +0300

    Delete 22.php

--- 22.php
+++
@@ -1,9 +0,0 @@
-<?php
-$uploaddir = 'tests/';
-if (move_uploaded_file($_FILES["userfile"]["tmp_name"], $uploaddir .
-    $_FILES['userfile']['name'])) {
-    print "Файл загружен.";
-} else {
-    print "Ошибка";
-}
-?>
